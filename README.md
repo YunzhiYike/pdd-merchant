346c2ec3f1349b1b41e463404431ff7f72869122
云知易客 <1647762341@qq.com>
2023-12-12 14:44:19 +0800

    feat:增加方法

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ compose yunzhiyike/pdd-merchant
 ```
 
 ## 使用说明
+### 1、登录流程
 ```php
+////////////////  登录步骤 /////////////////
 $pddMerchant = new PddMerchant(60, '远程拼多多加密服务地址', '远程拼多多加密服务token');
  // 发送验证码
 $pddMerchant->sendSmsCode('拼多多商家账号/子账号');
@@ -16,6 +18,46 @@ $pddMerchant->sendSmsCode('拼多多商家账号/子账号');
 $info = $pddMerchant->login('拼多多商家账号/子账号', '密码', '验证码');
 // 获取登录成功后的授权cookie
 $cookie = $pddMerchant->getCookie();
+
+
+////////////////  订单导出步骤 /////////////////
+// 创建订单导出任务
+$startTime = 1698768000;
+$endTime = 1702362388;
+$pddMerchant->createOrderTask($startTime, $endTime);
+// 获取订单导出任务列表
+$res = $pddMerchant->getOrderTaskList(1, 10);
+
+$orderTaskList = $res['pageItems'];
+foreach ($orderTaskList as $orderTask) {
+       // 根据刚刚导出的时间范围条件找到任务id
+       if ($orderTask['groupStartTime'] == $startTime && $orderTask['groupEndTime'] == $endTime) {
+       $taskId = $orderTask['id'];
+       // 根据找到的任务id获取真实的下载地址
+       $downloadUrl = $pddMerchant->getOrderTaskDownloadUrl($taskId);
+       var_dump($downloadUrl);
+       break;
+   }
+}
+
+////////////////  评价查询步骤 /////////////////
+```
+### 2、财务接口
+> 登录后还无法直接调用财务接口来获取财务数据还需要对登录cookie进行财务二次鉴权
+```php
+$pddMerchant = new PddMerchant(60, '远程拼多多加密服务地址', '远程拼多多加密服务token');
+ // 发送验证码
+$pddMerchant->sendSmsCode('拼多多商家账号/子账号');
+// 登录 返回登录账号信息和cookie
+$info = $pddMerchant->login('拼多多商家账号/子账号', '密码', '验证码');
+// 财务二次鉴权
+$pddMerchant->finances2Auth();
+// 调用获取财务月度数据报表
+$billingReport = $pddMerchant->queryMallBalanceMonthlySummary('2023-01', '2024-01');
+// 新建财务流水导出任务
+$pddMerchant->createBillDownloadTask(1698768000, time());
+// 获取财务流水任务列表（在这里可以下载财务流水）
+$taskList = $pddMerchant->queryBillDownloadTaskList(1, 10);
 ```
 
 # 免责声明
--- src/PddMerchant.php
+++ src/PddMerchant.php
@@ -123,7 +123,6 @@ class PddMerchant
         }
         $res = $res->getBody()->getContents();
         $res = json_decode($res, true);
-        var_dump($res);
         if ($res['errorCode'] != self::$REQUEST_OK) {
             throw new PddMerchantException($res['errorMsg'] ?? 'Pdd sendSmsCode Unknown Error');
         }
@@ -134,27 +133,201 @@ class PddMerchant
      * @throws Exception\PddEncryptionRemoteApiException
      * @throws PddMerchantException
      * @throws \GuzzleHttp\Exception\GuzzleException
-     *                                               获取验证码
+     *                                               财务二次授权
      */
     public function finances2Auth(): void
     {
         $at = $this->pddEncryptionRemoteApi->getAntiContent($this->userAgent);
-        $uri = self::$PDD_MERCHANT_HOST . '/janus/api/user/getLoginVerificationCode';
+        $uri = self::$PDD_MERCHANT_HOST . '/uranus/api/ticket/getJumpUrl/mallFinanceSiteBalanceBill';
         $headers = [
             'User-Agent' => $this->userAgent,
             'Anti-Content' => $at,
             'Content-Type' => 'application/json',
-            'Referer' => 'https://mms.pinduoduo.com/home/',
+            'Referer' => 'https://mms.pinduoduo.com/cashier/finance/payment-bills',
+            'Cookie' => $this->cookie,
         ];
         $body = [
-            'crawlerInfo' => $at,
-            'username' => $accountName,
+            'clientSource' => 1,
         ];
         $res = $this->client->post($uri, ['headers' => $headers, 'json' => $body])->getBody()->getContents();
         $res = json_decode($res, true);
         if ($res['errorCode'] != self::$REQUEST_OK) {
             throw new PddMerchantException($res['errorMsg'] ?? 'Pdd sendSmsCode Unknown Error');
         }
+        // 使用 parse_url 解析 URL
+        $urlComponents = parse_url($res['result']);
+        if (! isset($urlComponents['query'])) {
+            throw new PddMerchantException('参数提取失败');
+        }
+        parse_str($urlComponents['query'], $queryParams);
+        if (! isset($queryParams['ticket'])) {
+            throw new PddMerchantException('ticket参数不存在');
+        }
+        $ticket = $queryParams['ticket'];
+        // 二跳授权
+        $uri = 'https://cashier.pinduoduo.com/sherlock/api/auth/checkTicketV2';
+        $body = [
+            'ticket' => $ticket,
+        ];
+        $res = $this->client->post($uri, ['headers' => $headers, 'json' => $body]);
+        $respCookies = $res->getHeader('Set-Cookie') ?? [];
+        $res = $res->getBody()->getContents();
+        $res = json_decode($res, true);
+        if ($res['errorCode'] != self::$REQUEST_OK) {
+            throw new PddMerchantException($res['errorMsg'] ?? 'Pdd sendSmsCode Unknown Error');
+        }
+        $this->cookie = '';
+        foreach ($respCookies as $cookie) {
+            $this->cookie = $this->cookie . $cookie . '; ';
+        }
+    }
+
+    /**
+     * @param string $startMonth Y-m
+     * @param string $endMonth Y-m
+     * @throws Exception\PddEncryptionRemoteApiException
+     * @throws PddMerchantException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *                                               获取财务月度数据汇总
+     */
+    public function queryMallBalanceMonthlySummary(string $startMonth, string $endMonth): array
+    {
+        $uri = 'https://cashier.pinduoduo.com/templar/api/bill/queryMallBalanceMonthlySummary?__app_code=113';
+        $headers = [
+            'User-Agent' => $this->userAgent,
+            'Content-Type' => 'application/json',
+            'Referer' => 'https://cashier.pinduoduo.com/main/bills?tab=4001&__app_code=113',
+            'Cookie' => $this->cookie,
+        ];
+        $body = [
+            'beginMonth' => $startMonth,
+            'endMonth' => $endMonth,
+        ];
+        return $this->sendPost($uri, $headers, $body);
+    }
+
+    /**
+     * @param int $beginTime 开始时间戳
+     * @param int $endTime 结束时间戳
+     * @param null|string $orderSn 订单号
+     * @param null|mixed $minAmount 最小金额
+     * @param null|mixed $maxAmount 最大金额
+     * @throws Exception\PddEncryptionRemoteApiException
+     * @throws PddMerchantException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function createBillDownloadTask(int $beginTime, int $endTime, null|string $orderSn = null, mixed $minAmount = null, mixed $maxAmount = null): void
+    {
+        $uri = 'https://cashier.pinduoduo.com/templar/api/bill/createBillDownloadTask?__app_code=113';
+        $headers = [
+            'User-Agent' => $this->userAgent,
+            'Content-Type' => 'application/json',
+            'Referer' => 'https://cashier.pinduoduo.com/main/bills?tab=4001&__app_code=113',
+            'Cookie' => $this->cookie,
+        ];
+        $body = [
+            'beginTime' => $beginTime,
+            'endTime' => $endTime,
+            'groupType' => 0,
+            'classList' => [],
+            'minAmount' => $minAmount,
+            'maxAmount' => $maxAmount,
+            'orderSn' => $orderSn,
+        ];
+        $this->sendPost($uri, $headers, $body);
+    }
+
+    /**
+     * @throws Exception\PddEncryptionRemoteApiException
+     * @throws PddMerchantException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *                                               获取财务流水下载任务列表 配合createBillDownloadTask使用
+     */
+    public function queryBillDownloadTaskList(int $page = 1, int $pageSize = 10): array
+    {
+        $uri = 'https://cashier.pinduoduo.com/templar/api/bill/queryBillDownloadTaskList?__app_code=113';
+        $headers = [
+            'User-Agent' => $this->userAgent,
+            'Content-Type' => 'application/json',
+            'Referer' => 'https://cashier.pinduoduo.com/main/bills?tab=4001&__app_code=113',
+            'Cookie' => $this->cookie,
+        ];
+        $body = [
+            'page' => $page,
+            'size' => $pageSize,
+        ];
+        return $this->sendPost($uri, $headers, $body);
+    }
+
+    /**
+     * @param int $startTime 开始时间戳
+     * @param int $endTime 结束时间戳
+     * @throws Exception\PddEncryptionRemoteApiException
+     * @throws PddMerchantException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     *                                               创建订单导出任务
+     */
+    public function createOrderTask(int $startTime, int $endTime): array
+    {
+        $at = $this->pddEncryptionRemoteApi->getAntiContent($this->userAgent);
+        $uri = self::$PDD_MERCHANT_HOST . '/mars/shop/recentOrders/export/task/add';
+        $headers = [
+            'User-Agent' => $this->userAgent,
+            'Content-Type' => 'application/json',
+            'Referer' => 'https://mms.pinduoduo.com/orders/exportExcel',
+            'Cookie' => $this->cookie,
+        ];
+        $body = [
+            'orderType' => 0,
+            'afterSaleType' => 0,
+            'groupStartTime' => $startTime,
+            'groupEndTime' => $endTime,
+            'pageNumber' => 1,
+            'pageSize' => 20,
+            'templateName' => '自定义报表',
+            'titles' => [1, 2, 3, 13, 14, 15, 18, 86, 19, 20, 21, 22, 23, 77, 78, 79, 80, 24, 25, 26, 27, 28, 29, 61, 62, 70, 71, 72, 73, 81, 82, 84, 85, 88, 92, 4, 7, 5, 6, 94, 8, 9, 10, 11, 12, 30, 31, 34, 35, 36, 89, 87, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 54, 55, 56, 57, 58, 76, 59, 60, 64, 65, 66, 68, 69, 83, 90, 93],
+            'rememberTemplate' => true,
+            'hideRegionBlackDelayShipping' => false,
+            'crawlerInfo' => $at,
+        ];
+        return $this->sendPost($uri, $headers, $body, $at);
+    }
+
+    /**
+     * @param int $page 页数
+     * @param int $pageSize 页码
+     * @throws PddMerchantException
+     *                              获取订单导出任务列表
+     */
+    public function getOrderTaskList(int $page = 1, int $pageSize = 10): array
+    {
+        $uri = self::$PDD_MERCHANT_HOST . '/mars/shop/orders/export/task/list';
+        $headers = [
+            'User-Agent' => $this->userAgent,
+            'Content-Type' => 'application/json',
+            'Referer' => 'https://mms.pinduoduo.com/orders/exportExcel',
+            'Cookie' => $this->cookie,
+        ];
+        $body = [
+            'pageNumber' => $page,
+            'pageSize' => $pageSize,
+        ];
+        return $this->sendPost($uri, $headers, $body);
+    }
+
+    public function getOrderTaskDownloadUrl(int $id): string
+    {
+        $uri = self::$PDD_MERCHANT_HOST . '/mars/shop/orders/export/task/getUrl';
+        $headers = [
+            'User-Agent' => $this->userAgent,
+            'Content-Type' => 'application/json',
+            'Referer' => 'https://mms.pinduoduo.com/orders/exportExcel',
+            'Cookie' => $this->cookie,
+        ];
+        $body = [
+            'jobId' => $id,
+        ];
+        return $this->sendPost($uri, $headers, $body);
     }
 
     /**
@@ -221,5 +394,19 @@ class PddMerchant
     {
         $this->cookie = $cookies;
         return $this;
+    }
+
+    protected function sendPost(string $uri, array $headers, $body, string $at = ''): array|bool|string
+    {
+        if ($at === '') {
+            $at = $this->pddEncryptionRemoteApi->getAntiContent($this->userAgent);
+        }
+        $headers['Anti-Content'] = $at;
+        $res = $this->client->post($uri, ['headers' => $headers, 'json' => $body])->getBody()->getContents();
+        $res = json_decode($res, true);
+        if ($res['errorCode'] != self::$REQUEST_OK && $res['errorCode'] != 0) {
+            throw new PddMerchantException($res['errorMsg'] ?? 'Pdd sendSmsCode Unknown Error');
+        }
+        return $res['result'];
     }
 }
